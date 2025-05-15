60dbacfb29914f1aae5bc18a007d279a27641da1
Facundo Federico <facufede1998.dk@gmail.com>
2025-05-15 18:40:21 +0200

    Improve test

--- StateMachineLib.Tests/UnitTests/StatefulStateMachineTests.cs
+++ StateMachineLib.Tests/UnitTests/StatefulStateMachineTests.cs
@@ -5,43 +5,50 @@ namespace StateMachineLib.Tests.UnitTests;
 public class StatefulStateMachineTests
 {
     [Fact]
-    public void StatefulStateMachine()
+    public void StatefulStateMachine_Transitions()
     {
         // Arrange
-        var stateMachine = new StatefulStateMachine(new ConcreteState1());
+        var stateMachine = new StatefulStateMachine(new ConcreteState1(0));
         stateMachine
-            .Configure((ConcreteState1 s, Advance t) => new ConcreteState2())
-            .Configure((ConcreteState2 s, Advance t) => new ConcreteState3())
-            .Configure((ConcreteState2 s, GoBack t) => new ConcreteState1())
-            .Configure((ConcreteState3 s, GoBack t) => new ConcreteState2());
+            .Configure((ConcreteState1 s, Advance t) => new ConcreteState2(s.Iteration + 1))
+            .Configure((ConcreteState2 s, Advance t) => new ConcreteState3(s.Iteration + 1))
+            .Configure((ConcreteState2 s, GoBack t) => new ConcreteState1(s.Iteration + 1))
+            .Configure((ConcreteState3 s, GoBack t) => new ConcreteState2(s.Iteration + 1));
 
         // Act - Assert
         Assert.IsType<ConcreteState1>(stateMachine.State);
+        Assert.Equal(0, ((ConcreteState1)stateMachine.State).Iteration);
 
         // Advance
         stateMachine.Handle(new Advance());
         Assert.IsType<ConcreteState2>(stateMachine.State);
+        Assert.Equal(1, ((ConcreteState2)stateMachine.State).Iteration);
         
         stateMachine.Handle(new Advance());
         Assert.IsType<ConcreteState3>(stateMachine.State);
+        Assert.Equal(2, ((ConcreteState3)stateMachine.State).Iteration);
 
         stateMachine.Handle(new Advance());
         Assert.IsType<ConcreteState3>(stateMachine.State);
+        Assert.Equal(2, ((ConcreteState3)stateMachine.State).Iteration);
         
         // Go Back
         stateMachine.Handle(new GoBack());
         Assert.IsType<ConcreteState2>(stateMachine.State);
+        Assert.Equal(3, ((ConcreteState2)stateMachine.State).Iteration);
 
         stateMachine.Handle(new GoBack());
         Assert.IsType<ConcreteState1>(stateMachine.State);
+        Assert.Equal(4, ((ConcreteState1)stateMachine.State).Iteration);
         
         stateMachine.Handle(new GoBack());
         Assert.IsType<ConcreteState1>(stateMachine.State);
+        Assert.Equal(4, ((ConcreteState1)stateMachine.State).Iteration);
     }
 
-    private class ConcreteState1 : IState;
-    private class ConcreteState2 : IState;
-    private class ConcreteState3 : IState;
-    private class Advance : ITrigger;
-    private class GoBack : ITrigger;
+    private record ConcreteState1(int Iteration) : IState;
+    private record ConcreteState2(int Iteration) : IState;
+    private record ConcreteState3(int Iteration) : IState;
+    private record Advance : ITrigger;
+    private record GoBack : ITrigger;
 }
